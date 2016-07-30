a1e80ab335857f7606ca82facc107ada273302e8
oynek <oynek@users.noreply.github.com>
2016-07-30 12:55:37 +0200

    Update utils.py

--- pogom/utils.py
+++ pogom/utils.py
@@ -102,6 +102,8 @@ def get_args():
     parser.add_argument('--db-host', help='IP or hostname for the database')
     parser.add_argument('-wh', '--webhook', help='Define URL(s) to POST webhook information to',
                         nargs='*', default=False, dest='webhooks')
+    parser.add_argument('-t', '--threads', help='Number of search threads', required=False, type=int, default=5, dest='num_threads')
+    parser.add_argument('--pokel-pass', help='Password for Pokelizer database')
     parser.set_defaults(DEBUG=False)
 
     args = parser.parse_args()
